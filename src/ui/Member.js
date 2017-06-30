8da3e303d454f8273899dc94c12e4fe8e38e0812
German Gonzalez <nordicod3@gmail.com>
2017-06-29 21:44:46 -0500

    Using Mounting Lifecycle

--- src/ui/Member.js
+++ src/ui/Member.js
@@ -2,14 +2,21 @@ import FaShield from 'react-icons/lib/fa/shield';
 import React, { Component } from 'react';
 
 class Member extends Component {
+	
+	componentWillMount() {
+		this.style = {
+			backgroundColor: 'gray'
+		}
+	}
+
 	render(){
 
 		const {name, thumbnail, email, admin, makeAdmin} = this.props
 
 		return(
-			<div className="member">
+			<div className="member" style={this.style}>
 				<h1>{name} {(admin) ? <FaShield /> : null}</h1>
-				<a onclick={makeAdmin}>Make Admin</a>
+				<a onClick={makeAdmin}>Make Admin</a>
 				<img src={thumbnail} alt="profile" />
 				<p><a href={`mailto:${email}`}>{email}</a></p>
 			</div>
